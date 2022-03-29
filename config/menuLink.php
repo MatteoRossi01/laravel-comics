611013c4fa3d6084d85deef903e84f62fcfd5473
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-03-29 16:18:52 +0200

    first commit

---
+++ config/menuLink.php
@@ -0,0 +1,48 @@
+<?php
+
+    return [
+
+        [
+            'text' => 'CHARACTERS',
+            'active' => false,
+        ],
+        [
+            'text' => 'COMICS',
+            'active' => true,
+        ],
+        [
+            'text' => 'MOVIES',
+            'active' => false,
+        ],
+        [
+            'text' => 'TV',
+            'active' => false,
+        ],
+        [
+            'text' => 'GAMES',
+            'active' => false,   
+        ],
+        [
+            'text' => 'COLLECTIBLES',
+            'active' => false,
+        ],
+        [
+            'text' => 'VIDEOS',
+            'active' => false,
+        ],
+        [
+            'text' => 'FANS',
+            'active' => false,
+        ],
+        [
+            'text' => 'NEWS',
+            'active' => false,
+        ],
+        [
+            'text' => 'SHOP',
+            'active' => false,
+        ],
+        
+    ]
+
+?>
